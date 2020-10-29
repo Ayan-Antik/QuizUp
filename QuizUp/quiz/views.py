cf8e7c42f0fc33485d961b01b2f35ef8b5aa980c
Tanjim <tanjim.ak49@gmail.com>
2020-10-29 12:49:11 +0600

    add quiz statistics

--- QuizUp/quiz/views.py
+++ QuizUp/quiz/views.py
@@ -1,40 +1,94 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.db import connection
+from django.urls import reverse
+
+
+def has_played(quiz_id, player_id):
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT SCORE FROM QUIZ_ATTEMPT WHERE QUIZ_ID = %s AND PLAYER_ID = %s', [quiz_id, player_id])
+        return cursor.fetchone()
 
 
 def quiz_detail(request, quiz_id):
-    with connection.cursor() as cursor:
-        cursor.execute('SELECT * FROM QUIZ WHERE QUIZ_ID = %s', [quiz_id])
-        quiz = cursor.fetchone()
-        cursor.execute('SELECT NAME FROM TOPIC WHERE TOPIC_ID = (SELECT TOPIC_ID FROM QUIZ WHERE QUIZ_ID = %s)', [quiz_id])
-        topic = cursor.fetchone()[0]
-        cursor.execute('''SELECT USERNAME FROM USERS WHERE USER_ID = (SELECT MASTER_ID FROM QUIZ
-                                                WHERE QUIZ_ID = %s)''', [quiz_id])
-        quizmaster = cursor.fetchone()[0]
-        cursor.execute('''SELECT COUNT(*) FROM QUIZ_ATTEMPT GROUP BY QUIZ_ID
-                       HAVING QUIZ_ID = %s''', [quiz_id])
-        row = cursor.fetchone()
-        num_of_played = 0 if row is None else row
-        return render(request, 'quiz/quizDetail.html', {'quiz': quiz, 'topic': topic, 'quizmaster': quizmaster,
-                                                        'num_of_played': num_of_played})
+    if 'id' in request.session and request.session['type'] == "Player":
+        player_id = request.session.get('id')
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT * FROM TOPIC')
+            topics = cursor.fetchall()
+            cursor.execute('SELECT * FROM QUIZ WHERE QUIZ_ID = %s', [quiz_id])
+            quiz = cursor.fetchone()
+            cursor.execute('SELECT NAME FROM TOPIC WHERE TOPIC_ID = (SELECT TOPIC_ID FROM QUIZ WHERE QUIZ_ID = %s)',
+                           [quiz_id])
+            topic = cursor.fetchone()[0]
+            cursor.execute('''SELECT USERNAME FROM USERS WHERE USER_ID = (SELECT MASTER_ID FROM QUIZ
+                                                    WHERE QUIZ_ID = %s)''', [quiz_id])
+            quizmaster = cursor.fetchone()[0]
+            cursor.execute('''SELECT COUNT(*) FROM QUIZ_ATTEMPT GROUP BY QUIZ_ID
+                           HAVING QUIZ_ID = %s''', [quiz_id])
+            row = cursor.fetchone()
+            num_of_played = 0 if row is None else row[0]
+            row = has_played(quiz_id, player_id)
+            score = -1 if row is None else row[0]
+            if row is not None:
+                # JHAMELA ASE
+                cursor.execute('''SELECT * FROM QUESTION LEFT JOIN QUESTION_ATTEMPT USING (QUESTION_ID)
+                                WHERE QUIZ_ID = %s AND PLAYER_ID = %s''', [quiz_id, player_id])
+                results = cursor.fetchall()
+            else:
+                results = None
+            cursor.execute('''SELECT USERNAME, MAX(SCORE) FROM QUIZ_ATTEMPT JOIN USERS ON (PLAYER_ID = USER_ID)
+                           WHERE QUIZ_ID = %s GROUP BY USERNAME''', [quiz_id])
+            top_score = cursor.fetchone()
+            cursor.execute('SELECT AVG(SCORE) FROM QUIZ_ATTEMPT WHERE QUIZ_ID = %s', [quiz_id])
+            row = cursor.fetchone()
+            difficulty = '-'
+            if row[0] is None: # can do it in oracle function
+                if row[0] >= 20:
+                    difficulty = 'Easy'
+                elif row[0] >= 15:
+                    difficulty = 'Medium'
+                if row[0] > 20:
+                    difficulty = 'Hard'
+            return render(request, 'quiz/quizDetail.html', {'topics': topics, 'quiz': quiz, 'topic': topic,
+                                                            'quizmaster': quizmaster, 'num_of_played': num_of_played,
+                                                            'score': score, 'top_score': top_score, 'results': results,
+                                                            'difficulty': difficulty})
+    else:
+        return HttpResponseRedirect(reverse('login'))
 
 
 def play_quiz(request, quiz_id):
-    with connection.cursor() as cursor:
-        cursor.execute('SELECT * FROM QUIZ WHERE QUIZ_ID = %s', [quiz_id])
-        quiz = cursor.fetchone()
-        cursor.execute('SELECT * FROM QUESTION WHERE QUIZ_ID = %s', [quiz_id])
-        questions = cursor.fetchall()
-    return render(request, 'quiz/quiz.html', {'quiz': quiz, 'questions': questions})
+    if 'id' in request.session and request.session['type'] == "Player":
+        player_id = request.session.get('id')
+        if has_played(quiz_id, player_id) is not None:
+            return HttpResponseRedirect(reverse('quiz_detail', args=quiz_id))
+        with connection.cursor() as cursor:
+            cursor.execute('SELECT * FROM QUIZ WHERE QUIZ_ID = %s', [quiz_id])
+            quiz = cursor.fetchone()
+            cursor.execute('SELECT * FROM QUESTION WHERE QUIZ_ID = %s', [quiz_id])
+            questions = cursor.fetchall()
+            cursor.execute('INSERT INTO QUIZ_ATTEMPT VALUES(%s, %s, 0)', [quiz_id, player_id])
+        return render(request, 'quiz/quiz.html', {'quiz': quiz, 'questions': questions})
+    else:
+        return HttpResponseRedirect(reverse('login'))
 
 
 def update_score(request):
-    with connection.cursor() as cursor:
-        question_id = request.POST.get('question_id')
-        score = request.POST.get('score')
-        choice = request.POST.get('choice')
-        print(question_id)
-        print(score)
-        print(choice)
-        return HttpResponse('')
+    if 'id' in request.session and request.session['type'] == "Player":
+        player_id = request.session.get('id')
+        with connection.cursor() as cursor:
+            question_id = request.POST.get('question_id')
+            quiz_id = request.POST.get('quiz_id')
+            score = request.POST.get('score')
+            choice = request.POST.get('choice')
+            cursor.execute('INSERT INTO QUESTION_ATTEMPT VALUES(%s, %s, %s, %s)',
+                           [question_id, player_id, score, choice])
+            row = has_played(quiz_id, player_id)
+            prev_score = row[0]
+            cursor.execute('UPDATE QUIZ_ATTEMPT SET SCORE = %s WHERE QUIZ_ID = %s AND PLAYER_ID = %s',
+                           [prev_score + int(score), quiz_id, player_id])
+            return HttpResponse('')
+    else:
+        return HttpResponseRedirect(reverse('login'))
--- QuizUp/topic/views.py
+++ QuizUp/topic/views.py
@@ -3,41 +3,44 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.db import connection
-from django.forms.formsets import formset_factory
 from django.http import JsonResponse
 
 
 def update_follow(request):
-    with connection.cursor() as cursor:
-        topic_id = request.POST.get('topic_id')
-        is_follow = request.POST.get('is_follow')
-        if is_follow == 'Follow':
-            cursor.execute('INSERT INTO TOPIC_FOLLOW VALUES(%s, %s)', [topic_id, player_id])
-            is_follow = 'Followed'
-        else:
-            cursor.execute('DELETE FROM TOPIC_FOLLOW WHERE TOPIC_ID = %s AND FOLLOWER_ID = %s', [topic_id, player_id])
-            is_follow = 'Follow'
-        return JsonResponse({'is_follow': is_follow})
+    if 'id' in request.session and request.session['type'] == "Player":
+        player_id = request.session.get('id')
+        with connection.cursor() as cursor:
+            topic_id = request.POST.get('topic_id')
+            is_follow = request.POST.get('is_follow')
+            if is_follow == 'Follow':
+                cursor.execute('INSERT INTO TOPIC_FOLLOW VALUES(%s, %s)', [topic_id, player_id])
+                is_follow = 'Following'
+            else:
+                cursor.execute('DELETE FROM TOPIC_FOLLOW WHERE TOPIC_ID = %s AND FOLLOWER_ID = %s', [topic_id, player_id])
+                is_follow = 'Follow'
+            return JsonResponse({'is_follow': is_follow})
+    else:
+        return HttpResponseRedirect(reverse('login'))
 
 
 def topic_detail(request, topic_id):
     if 'id' in request.session and request.session['type'] == "Player":
         player_id = request.session.get('id')
-        print(player_id)
-        print(request.session['type'])
         with connection.cursor() as cursor:
+            cursor.execute('SELECT * FROM TOPIC')
+            topics = cursor.fetchall()
             cursor.execute('SELECT * FROM TOPIC WHERE TOPIC_ID = %s', [topic_id])
             topic = cursor.fetchone()
-            cursor.execute('SELECT * FROM TOPIC_FOLLOW WHERE TOPIC_ID = %s AND FOLLOWER_ID = 1', [topic_id])
+            cursor.execute('SELECT * FROM TOPIC_FOLLOW WHERE TOPIC_ID = %s AND FOLLOWER_ID = %s', [topic_id, player_id])
             is_follow = 'Follow' if cursor.fetchone() is None else 'Following'
             cursor.execute('SELECT COUNT(*) FROM TOPIC_FOLLOW GROUP BY TOPIC_ID HAVING TOPIC_ID = %s', [topic_id])
             row = cursor.fetchone()
             num_of_followers = 0 if row is None else row[0]
-            cursor.execute('SELECT TOPIC_RANK FROM TOPIC_ATTEMPT WHERE TOPIC_ID = %s AND PLAYER_ID = 1', [topic_id])
+            cursor.execute('SELECT TOPIC_RANK FROM TOPIC_ATTEMPT WHERE TOPIC_ID = %s AND PLAYER_ID = %s', [topic_id, player_id])
             row = cursor.fetchone()
             rank = '-' if row is None else row[0]
             cursor.execute('''SELECT COUNT(*) FROM QUIZ_ATTEMPT QA JOIN QUIZ Q USING (QUIZ_ID)
-                                WHERE Q.TOPIC_ID = %s GROUP BY QA.PLAYER_ID HAVING QA.PLAYER_ID = 1''', [topic_id])
+                                WHERE Q.TOPIC_ID = %s GROUP BY QA.PLAYER_ID HAVING QA.PLAYER_ID = %s''', [topic_id, player_id])
             row = cursor.fetchone()
             quiz_completed = 0 if row is None else row[0]
             cursor.execute('SELECT COUNT(*) FROM QUIZ GROUP BY TOPIC_ID HAVING TOPIC_ID = %s', [topic_id])
@@ -47,7 +50,7 @@ def topic_detail(request, topic_id):
             quiz_list = cursor.fetchall()
             if quiz_list is None:
                 quiz_list = []
-            return render(request, 'topic/topic.html', {'topic': topic, 'is_follow': is_follow,
+            return render(request, 'topic/topic.html', {'topics': topics, 'topic': topic, 'is_follow': is_follow,
                                                         'num_of_followers': num_of_followers, 'rank': rank,
                                                         'quiz_completed': quiz_completed, 'total_quiz': total_quiz,
                                                         'quiz_list': quiz_list})
